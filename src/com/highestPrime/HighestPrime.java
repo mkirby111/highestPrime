b0f24622df5f69fca490dcf62689204f9daa263c
Mark Kirby <mkirby111@gmail.com>
2016-03-29 15:14:06 -0700

    First commit

---
+++ src/com/highestPrime/HighestPrime.java
@@ -0,0 +1,10 @@
+package com.highestPrime;
+
+/**
+ * Created by mkirby on 3/29/16.
+ */
+public class HighestPrime {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/highestPrime/HighestPrimeTest.java
@@ -0,0 +1,10 @@
+package com.highestPrime;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mkirby on 3/29/16.
+ */
+public class HighestPrimeTest {
+
+}
