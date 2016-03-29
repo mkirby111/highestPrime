a7667947964350051822e3d44b5d64c79e62c40a
Mark Kirby <mkirby111@gmail.com>
2016-03-29 16:28:16 -0700

    Built simple timer with time printed for 5 seconds

--- src/com/highestPrime/HighestPrime.java
+++ src/com/highestPrime/HighestPrime.java
@@ -4,7 +4,17 @@ package com.highestPrime;
  * Created by mkirby on 3/29/16.
  */
 public class HighestPrime {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        long startTime = System.currentTimeMillis();
+        long duration = 5000;
+        long totalTime =  startTime + duration;
+        int seconds = 0;
 
+        while (System.currentTimeMillis() <= totalTime) {
+            System.out.println("Time: " + seconds + " seconds");
+            Thread.sleep(1000);
+            seconds++;
+        }
+        System.out.println("Total Time: " +  seconds + " seconds");
     }
 }
--- src/com/highestPrime/HighestPrimeTest.java
+++ src/com/highestPrime/HighestPrimeTest.java
@@ -1,10 +1,12 @@
 package com.highestPrime;
 
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
  * Created by mkirby on 3/29/16.
  */
 public class HighestPrimeTest {
+
 
 }
