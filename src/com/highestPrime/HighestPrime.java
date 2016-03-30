310e0d126567080f9148eb97a731100c743d08db
Mark Kirby <mkirby111@gmail.com>
2016-03-29 20:53:35 -0700

    Fixed time and maxPrime output so that progress is tracked to a reasonable degree. Also displays final maxPrime.

--- src/com/highestPrime/HighestPrime.java
+++ src/com/highestPrime/HighestPrime.java
@@ -1,41 +1,54 @@
 package com.highestPrime;
 
 /**
- * Created by mkirby on 3/29/16.
+ *
  */
 public class HighestPrime {
-    private static long startTime = System.currentTimeMillis();
-    private static long duration = 5000;
-    private static long totalTime =  startTime + duration;
-    private static int seconds = 0;
-    private static int num = 1;
+    private static long time = System.currentTimeMillis();
+    private static long duration = 60000;
+    private static long totalTime =  time + duration;
 
     public static void main(String[] args) throws InterruptedException {
         int maxPrime = 2;
+        int num = 1;
 
         while (System.currentTimeMillis() <= totalTime) {
+            int previousMaxPrime = maxPrime;
             if (isPrime(num) == true)
-                maxPrime =num;
-            printProgress(maxPrime);
+                maxPrime = num;
+            if ((System.currentTimeMillis()%1000 == 0) && (previousMaxPrime < maxPrime))
+                printProgress(maxPrime);
             num++;
         }
-        System.out.println("\n======================================================\n");
+        printFinal(maxPrime, getElapsedTime());
+    }
+
+    /**
+     *
+     * @param maxPrime
+     * @param seconds
+     */
+    private static void printFinal(int maxPrime, int seconds) {
+        System.out.println("===============================================");
         System.out.println("Total Time: " +  seconds + " seconds");
         System.out.println("Highest Prime: " + maxPrime);
+        System.out.println("===============================================");
     }
 
     private static void printProgress(int maxPrime) throws InterruptedException {
-            System.out.println("Time: " + seconds + " seconds\t" + "Highest Prime: " + maxPrime);
-            Thread.sleep(1000);
-            seconds++;
+        System.out.println("Time: " + getElapsedTime() + " seconds\t\t" + "Highest Prime: " + maxPrime);
+    }
+
+    private static int getElapsedTime() {
+        return (int) ((System.currentTimeMillis()-time)/1000);
     }
 
     public static boolean isPrime(int n) {
         //check for negative, 0, or 1
         if (n<=1) return false;
-        //check for 2 or 3
+            //check for 2 or 3
         else if (n <= 3) return true;
-        //check if multiple of 2
+            //check if multiple of 2
         else if (n%2==0) return false;
         //check the odds greater than 3
         for (int i = 3; i*i<=n; i+=2)
