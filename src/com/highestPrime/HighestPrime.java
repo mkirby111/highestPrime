6895aa6800ed79d72d38fb1ef8bd9894351cee70
Mark Kirby <mkirby111@gmail.com>
2016-03-29 17:06:04 -0700

    Created isPrime() method with test (test behaves as expected)

--- src/com/highestPrime/HighestPrime.java
+++ src/com/highestPrime/HighestPrime.java
@@ -17,4 +17,17 @@ public class HighestPrime {
         }
         System.out.println("Total Time: " +  seconds + " seconds");
     }
+
+    public static boolean isPrime(int n) {
+        //check for negative, 0, or 1
+        if (n<=1) return false;
+        //check for 2 or 3
+        else if (n <= 3) return true;
+        //check if multiple of 2
+        else if (n%2==0) return false;
+        //check the odds greater than 3
+        for (int i = 3; i*i<=n; i+=2)
+            if (n%i==0) return false;
+        return true;
+    }
 }
--- src/com/highestPrime/HighestPrimeTest.java
+++ src/com/highestPrime/HighestPrimeTest.java
@@ -7,6 +7,9 @@ import static org.junit.Assert.*;
  * Created by mkirby on 3/29/16.
  */
 public class HighestPrimeTest {
-
+    @Test
+    public void testPrimeNumber() {
+        assertTrue(HighestPrime.isPrime(7));
+    }
 
 }
